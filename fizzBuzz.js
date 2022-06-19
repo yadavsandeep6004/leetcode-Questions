9dd27f635c145e6852f0f02c9996bdc351e3fe8b
Sandeep <yadavsandeep6004@gmail.com>
2022-06-19 08:28:53 +0530

    frist commit in leetcode

---
+++ fizzBuzz.js
@@ -0,0 +1,19 @@
+
+//day 1. https://leetcode.com/problems/fizz-buzz/
+var fizzBuzz = function(n) {
+    let arr=[];
+    for(let i=1;i<=n;i++){
+        if(i%15===0){
+        arr.push("FizzBuzz")
+      }else if(i%5===0){
+        arr.push("Buzz")
+      }else if(i%3===0){
+        arr.push("Fizz")
+      }else{
+        arr.push(i+"")
+      }
+    }
+    return arr;
+  };
+
+  console.log(fizzBuzz(15));
